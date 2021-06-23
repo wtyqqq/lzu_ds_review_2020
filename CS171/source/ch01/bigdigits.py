c6241da339852ffd4184249629a548f6fd4c0661
幼稚的小樱桃 <623383371@qq.com>
2021-06-23 15:23:08 +0800

    add something

---
+++ CS171/source/ch01/bigdigits.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# Copyright (c) 2008-11 Qtrac Ltd. All rights reserved.
+# This program or module is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version. It is provided for educational
+# purposes and is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+# buYunHui  add annotation
+import sys
+
+Zero = ["  ***  ",
+        " *   * ",
+        "*     *",
+        "*     *",
+        "*     *",
+        " *   * ",
+        "  ***  "]
+One = [" * ", "** ", " * ", " * ", " * ", " * ", "***"]
+Two = [" *** ", "*   *", "*  * ", "  *  ", " *   ", "*    ", "*****"]
+Three = [" *** ", "*   *", "    *", "  ** ", "    *", "*   *", " *** "]
+Four = ["   *  ", "  **  ", " * *  ", "*  *  ", "******", "   *  ",
+        "   *  "]
+Five = ["*****", "*    ", "*    ", " *** ", "    *", "*   *", " *** "]
+Six = [" *** ", "*    ", "*    ", "**** ", "*   *", "*   *", " *** "]
+Seven = ["*****", "    *", "   * ", "  *  ", " *   ", "*    ", "*    "]
+Eight = [" *** ", "*   *", "*   *", " *** ", "*   *", "*   *", " *** "]
+Nine = [" ****", "*   *", "*   *", " ****", "    *", "    *", "    *"]
+
+Digits = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]  # 列表的列表  二维数组？
+
+try:
+    digits = sys.argv[1]
+    row = 0
+    while row < 7:
+        line = ""
+        column = 0
+        while column < len(digits):
+            number = int(digits[column])  # 说明python输入 被处理为字符序列
+            #print(number)
+            digit = Digits[number]
+            # print(digit)
+            line += digit[row] + "  "    # 取指定列表的元素并拼接成要求格式（One。。。）
+            print(line)
+            column += 1
+        # print(line)
+        row += 1
+except IndexError:                       #  下标异常
+    print("usage: bigdigits.py <number>")
+except ValueError as err:
+    print(err, "in", digits)
+
+
+
